cb0dfea94425febb9296dbc161cf1ac33b704403
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-03-06 09:54:13 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
-import socket 
+import socket
 
-port = 10500 #porta de conexão
-dest = 'localhost' #'192.168.246.55' #ip da maquina
+port = 10500
 
-#msg = '90 anos, eu faço a minha calçada, eu faço e ele quebra, eu faço e ele quebra denovo'
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', port))
+server.listen()
 
-print(f'=== Conectando ao servidor {dest}:{port} ====')
-client.connect((dest,port))
+while True:
+    print(f'=== Servidor aguardando conexões na porta {port}===')
+    conn, addr = server.accept()
+    print(f'Conexão recebida de {addr}')
 
-while True: 
+
+#Recebe os dados enviados pelo cliente
+    data = conn.recv(4096)
+    msg = data.decode()
+    #print(f'Recebido: {data.decode()}')
+
     msg = input(': ')
-    client.send(msg.encode())
-
-    msg = client.recv(4096)
-    #print(f'Servidor: {msg.decode()}')
-
-client.close()
+    conn.send(msg.encode())
+#Encerra a conexão
+conn.close()
