927d6be0ace344075cf53b3cdc6ec53faf8a1463
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-04-11 14:32:04 -0300

    Update http_server_lib_threads.py

--- AtvdThreads/http_server_lib_threads.py
+++ AtvdThreads/http_server_lib_threads.py
@@ -1,1 +1,32 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
 
+addr = '0.0.0.0'
+port = 8084
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        self.send_response(200)
+        self.send_header('COntent-type','text/html')
+        self.end_headers()
+
+        content = "<html><body>Hello world</body></html>"
+
+        self.wfile.write(bytes(content, 'utf-8'))
+        return
+
+class ThreadedHTTPServer(ThreadingMixIn, Handler):
+    pass
+
+def main():
+    try:
+        server = ThreadedHTTPServer((addr, port), Handler)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Exiting server')
+        server.socket.close()
+
+
+if __name__ == '__main__':
+    main()
